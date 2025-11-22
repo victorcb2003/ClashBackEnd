320dd289886906185f7481ed07d45363dffbe341
victorcb2003 <187378295+victorcb2003@users.noreply.github.com>
2025-11-22 09:57:47 +0100

    ajout form + mail

--- app/class/form.class.js
+++ app/class/form.class.js
@@ -1,8 +1,30 @@
 const dbconnection = require('../db/connection');
+const Form = require("../mail/form.mail")
 
 module.exports = class Form {
 
     static post(req,res){
-        
+        const connection = dbconnection()
+        const sql = "INSERT INTO Forms (prenom, nom, email) values (?,?,?)"
+        values = [req.body.prenom, req.body.nom, req.body.email]
+
+        connection.execute(sql,values,(err,results,fields)=>{
+            if (err){
+                return res.status(400).send({message : err.message})
+            }
+            Form.sendMail(req,res)
+        })
+    }
+
+    static get(req,res){
+        const connection = dbconnection()
+        const sql = "Select * from Forms"
+
+        connection.execute(sql,(err,results,fields)=>{
+            if (err){
+                return res.status(400).send({message : err.message})
+            }
+            return res.status(200).send({message : results})
+        })
     }
 }
---
+++ app/mail/form.mail.js
@@ -0,0 +1,42 @@
+const nodemailer = require("nodemailer");
+const fs = require("fs").promises;
+const Handlebars = require("handlebars");
+const Token = require("./token.class.js");
+
+module.exports = class Selectionneur extends User {
+  static sendMail(req, res) {
+    let transporter = nodemailer.createTransport({
+      sendmail: true,
+      newline: "unix",
+      path: "/usr/sbin/sendmail",
+    });
+
+    // 1. Lire le template
+    const templateSource = await fs.readFile("./emails/welcome.hbs", "utf8");
+
+    // 2. Compiler le template
+    const template = Handlebars.compile(templateSource);
+
+    // 3. Générer le HTML final avec variables
+    const html = template({
+      prenom: req.body.prenom,
+      confirmUrl: `https://clashofleagues.fr/confirm/${Token.generateToken({id: body.req.email})}`,
+    });
+
+    // 4. Préparer l’email
+    const mailOptions = {
+      from: '"Clash of Leagues" <no-reply@clashofleagues.fr>',
+      to: req.body.email,
+      subject: "Bienvenue sur Clash of Leagues !",
+      html,
+    };
+
+    // Envoi du mail
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        return res.status(400).send({message : error.message});
+      }
+      return res.status(200).send({message : "L'email a bien été envoyé. "+info.messageId})
+    });
+  }
+};
