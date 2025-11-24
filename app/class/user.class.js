8a4facc7eef6a691431bebb49e583268c5fadfd0
victorcb2003 <victor.combemorel@gmail.com>
2025-11-24 12:23:32 +0100

    correction form

--- app/class/user.class.js
+++ app/class/user.class.js
@@ -90,16 +90,15 @@ module.exports = class User {
                         res.status(400).send({ message: "Erreur lors de la récupération du type d'utilisateur." + err.message });
                     }
                     if (results[0] != undefined) {
+                        const token = Token.generateToken({id: user.id,type: element})
                         res.status(200).send({
-                            message: "Login réussi !"
+                            message: "Login réussi !",
+                            token
                         })
                         .cookie(
                             "token", 
-                            Token.generateToken({id: user.id,type: element}),
-                            {
-                            httpOnly: false,       
-                            secure: false,         
-                            sameSite: "strict",   
+                            token,
+                            {  
                             maxAge: 60 * 60 * 1000 
                             }
                         );
