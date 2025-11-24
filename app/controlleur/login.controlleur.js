ebcb5507842608bf7542612f519e11003ebc4395
victorcb2003 <victor.combemorel@gmail.com>
2025-11-24 14:09:24 +0100

    correction form

--- app/controlleur/login.controlleur.js
+++ app/controlleur/login.controlleur.js
@@ -32,7 +32,7 @@ exports.delete = (req,res) =>{
 };
 
 exports.test = (req, res) => {
-  console.log(req.rawHeaders,req.header.cookie);
+  console.log(req.headers,req.cookie);
     if (!req.tokenData) {
         res.status(401).send({ message: "Token invalide" });
     } else {
