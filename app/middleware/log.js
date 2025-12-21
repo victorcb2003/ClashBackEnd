a1274a280662ac5bba655d4c239a319898149be5
victorcb2003 <victor.combemorel@gmail.com>
2025-12-21 11:15:21 +0100

    middleware log

--- app/middleware/log.js
+++ app/middleware/log.js
@@ -6,9 +6,9 @@ module.exports = app => {
 
         res.on("finish", () => {
             
-            const couleur = req.method[0] == "4" ? "\e[31m" : "\e[32m"
+            const couleur = req.method[0] == "4" ? "\x1b[31m" : "\x1b[32m"
 
-            console.log(`${couleur} [FINISH] ${req.method} \e[37m ${req.url} -> ${res.statusCode} (${Date.now() - start}ms)`)
+            console.log(`${couleur} [FINISH] ${req.method} \x1b[37m ${req.url} -> ${res.statusCode} (${Date.now() - start}ms)`)
         })
 
         next()
