b7547d616ac2d076ccb9d65abb170de0d178be3b
Victor Combemorel--Bluntz <victor.combemorel@gmai.com>
2026-01-21 15:27:40 +0100

    user/me requete sql

--- app/class/user.class.js
+++ app/class/user.class.js
@@ -145,13 +145,7 @@ module.exports = class User {
         const sql = `select id,prenom,nom,email from User where id = ?;
         select Matchs.date_heure,Matchs.lieu,Matchs.Equipe1_id,Matchs.Equipe2_id,Matchs.score,Matchs.Tournois_id from Match 
         inner join Joueurs
-        inner join Selectionneur
-        inner join Organisateur
-        inner join Equipes
-        inner join Tournois
-        where Joueurs.User_id = ? AND (Joueurs.Equipe_id = Matchs.Equipe1_id OR Joueurs.Equipe_id = Matchs.Equipe2_id) OR
-        Selectionneur.User_id = ? AND Equipes.Selectionneurs.User_id OR
-        Organisateur.User_id = ? AND Tournois.Organisateur_id = Organisateur.id
+        where Joueurs.User_id = ? AND (Joueurs.Equipe_id = Matchs.Equipe1_id OR Joueurs.Equipe_id = Matchs.Equipe2_id)
         `
 
         const value = [req.tokenData.id,req.tokenData.id,req.tokenData.id]
