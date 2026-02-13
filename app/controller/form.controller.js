6f676ffa201aeb8300717e1174dde3eed457ac2e
Victor Combemorel--Bluntz <victor.combemorel@gmai.com>
2026-02-13 16:02:12 +0100

    modification controller form

--- app/controller/form.controller.js
+++ app/controller/form.controller.js
@@ -22,7 +22,10 @@ exports.get = (req, res)=>{
 
 exports.confirm = (req, res) => {
     if (!req.body.token) {
-        return res.status(400).send({error: "Accès non autorisé."});
+        return res.status(400).send({error: "Le token est pas dans le body"});
+    }
+    if (!req.body.password) {
+        return res.status(400).send({error: "Le password n'est pas dans le body"});
     }
 
     Form.confirm(req, res)
