60e28e26d76eb1487c87cab631c836ab17b66bcd
Victor Combemorel--Bluntz <victor.combemorel@gmai.com>
2026-02-23 18:34:17 +0100

    ajout de l'id de Match dans user.info

--- app/class/user.class.js
+++ app/class/user.class.js
@@ -139,7 +139,7 @@ module.exports = class User {
 
         let sql = `
         select id,prenom,nom,email,img_url from User where id = ?;
-        select Matchs.date_heure,Matchs.lieu,Matchs.Equipe1_id,Matchs.Equipe2_id,Matchs.Tournois_id from Matchs
+        select Matchs.id,Matchs.date_heure,Matchs.lieu,Matchs.Equipe1_id,Matchs.Equipe2_id,Matchs.Tournois_id from Matchs
         left join Joueurs ON Joueurs.Equipe_id = Matchs.Equipe1_id OR Joueurs.Equipe_id = Matchs.Equipe2_id
         left join Equipes ON Joueurs.Equipe_id = Equipes.id
         left join Tournois ON Matchs.Tournois_id = Tournois.id
