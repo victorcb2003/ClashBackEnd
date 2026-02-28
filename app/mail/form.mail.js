a7c7322446353890a46bbd0a60071bac7874d49b
Victor Combemorel--Bluntz <victor.combemorel@gmai.com>
2026-02-28 09:27:34 +0100

    bug

--- app/mail/form.mail.js
+++ app/mail/form.mail.js
@@ -3,92 +3,52 @@ const Handlebars = require("handlebars");
 const Token = require("../class/token.class");
 
 module.exports = class Mail {
-  static async sendMail(req, res, mailOptions) {
+  static async sendMail(req, res) {
     let transporter = nodemailer.createTransport({
       sendmail: true,
       newline: "unix",
       path: "/usr/sbin/sendmail",
     });
 
+    const templateSource = `
+      <!DOCTYPE html>
+<html>
+  <body>
+    <h1>Bienvenue {{prenom}} !</h1>
+
+    <p>Merci pour votre inscription sur Clash of Leagues.</p>
+
+    <p>
+      Pour confirmer votre compte, cliquez ici :
+
+      <a href="{{confirmUrl}}" title="">Confirmer mon compte</a>
+    </p>
+
+    <p>À bientôt,<br>L'équipe Clash of Leagues</p>
+  </body>
+</html>
+    `;
+
+    const template = Handlebars.compile(templateSource);
+
+    const html = template({
+      prenom: req.body.prenom,
+      confirmUrl: `https://clashofleagues.fr/confirm.html?token=${Token.generateToken({ email: req.body.email, prenom: req.body.prenom, nom: req.body.nom, type: req.body.type }, '30m')}`,
+    });
+
+    const mailOptions = {
+      from: '"Clash of Leagues" <no-reply@clashofleagues.fr>',
+      to: req.body.email,
+      subject: "Bienvenue sur Clash of Leagues !",
+      html,
+    };
+
+    // Envoi du mail
     transporter.sendMail(mailOptions, (error, info) => {
       if (error) {
-        return res.status(500).send({ message: error.message });
+        return res.status(400).send({ message: error.message });
       }
-      return res
-        .status(200)
-        .send({ message: "L'email a bien été envoyé. " + info.messageId });
+      return res.status(200).send({ message: "L'email a bien été envoyé. " + info.messageId })
     });
-  }
-
-  static async sendResetPassword(req, res) {
-    const templateSource = `
-      <!DOCTYPE html>
-<html>
-  <body>
-    <h1>Bienvenue {{prenom}} !</h1>
-
-    <p>Merci pour votre inscription sur Clash of Leagues.</p>
-
-    <p>
-      Pour confirmer votre compte, cliquez ici :
-
-      <a href="{{confirmUrl}}" title="">Confirmer mon compte</a>
-    </p>
-
-    <p>À bientôt,<br>L'équipe Clash of Leagues</p>
-  </body>
-</html>
-    `;
-
-    const template = Handlebars.compile(templateSource);
-
-    const html = template({
-      prenom: req.body.prenom,
-      confirmUrl: `https://clashofleagues.fr/confirmation?token=${Token.generateToken({ email: req.body.email, prenom: req.body.prenom, nom: req.body.nom, type: req.body.type }, "30m")}`,
-    });
-
-    const mailOptions = {
-      from: '"Clash of Leagues" <no-reply@clashofleagues.fr>',
-      to: req.body.email,
-      subject: "Bienvenue sur Clash of Leagues !",
-      html,
-    };
-    Mail.sendMail(req, res, mailOptions);
-  }
-
-  static async sendConfirmationEmail(req, res) {
-    const templateSource = `
-      <!DOCTYPE html>
-<html>
-  <body>
-    <h1>Bienvenue {{prenom}} !</h1>
-
-    <p>Merci pour votre inscription sur Clash of Leagues.</p>
-
-    <p>
-      Pour confirmer votre compte, cliquez ici :
-
-      <a href="{{confirmUrl}}" title="">Confirmer mon compte</a>
-    </p>
-
-    <p>À bientôt,<br>L'équipe Clash of Leagues</p>
-  </body>
-</html>
-    `;
-
-    const template = Handlebars.compile(templateSource);
-
-    const html = template({
-      prenom: req.body.prenom,
-      confirmUrl: `https://clashofleagues.fr/confirmation?token=${Token.generateToken({ email: req.body.email, prenom: req.body.prenom, nom: req.body.nom, type: req.body.type }, "30m")}`,
-    });
-
-    const mailOptions = {
-      from: '"Clash of Leagues" <no-reply@clashofleagues.fr>',
-      to: req.body.email,
-      subject: "Bienvenue sur Clash of Leagues !",
-      html,
-    };
-    Mail.sendMail(req, res, mailOptions);
   }
 };
