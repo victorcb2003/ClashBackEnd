0f7657451028b391c1969e88e2fbdb8c67e57677
Victor Combemorel--Bluntz <victor.combemorel@gmai.com>
2026-06-17 22:32:04 +0200

    debug

--- app/class/form.class.js
+++ app/class/form.class.js
@@ -33,9 +33,7 @@ module.exports = class Form {
             password: req.body.password
         });
 
-        console.log(token.type)
-
-        if (token.type != "Joueur" && token.type != "Selectionneurs" && token.type != "Organisateur") {
+        if (token.type != "Joueurs" && token.type != "Selectionneurs" && token.type != "Organisateur") {
             return res.status(400).send({ error: "Type d'utilisateur invalide." });
         }
 
